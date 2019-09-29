1687a8980cfd0e430a841fbf18821f6b74152f03
ChanhyukByeon <bdu00chch@naver.com>
2019-09-29 22:31:42 +0900

    modal size up

--- resources/views/components/information.blade.php
+++ resources/views/components/information.blade.php
@@ -46,7 +46,7 @@
 
 {{-- 이미지 자세히보기 모달 --}}
 <div class="modal fade" id="imageDetailModal{{ $information->id }}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="imageDetailModalTitle{{ $information->id }}">이미지 자세히 보기</h5>
