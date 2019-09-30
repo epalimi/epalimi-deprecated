e7b141a6ab15184d429a558f09a5ae03beb124ce
ChanhyukByeon <bdu00chch@naver.com>
2019-09-30 23:11:11 +0900

    home carousel

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -10,10 +10,10 @@
         position: absolute;
         top: 0;
         right: 0;
-        width: 100%;
+        width: auto;
         height: 100%;
         padding: 3.5rem 2.5rem 1rem 2rem;
-        background-color: rgba(0, 0, 0, .75);
+        background-color: rgba(0, 0, 0, .65);
         color: white;
         line-height: 1.35;
     }
@@ -27,15 +27,37 @@
         text-decoration: none;
     }
 
-    @media (min-width: 576px) {
-        .card-columns {
-            column-count: 2;
+    .organization-title {
+        font-size: 1.3rem;
+    }
+
+    /* max-width media */
+    @media (max-width: 768px) {
+        .carousel-text {
+            width: 100%;
+        }
+
+        .carousel-bgimg {
+            height: 460px !important;
+        }
+
+        .carousel-title {
+            font-size: 30px !important;
+        }
+
+        .carousel-description {
+            font-size: 13px !important;
+        }
+
+        .carousel-bottom {
+            font-size: 14px !important;
         }
     }
 
-    @media (min-width: 768px) {
-        .carousel-text {
-            width: auto;
+    /* min-width media */
+    @media (min-width: 576px) {
+        .card-columns {
+            column-count: 2;
         }
     }
 
@@ -70,17 +92,17 @@
             <div class="carousel-item active">
                 <div class="carousel-bgimg position-relative" style="height: 500px; background: no-repeat center/cover url('images/main-1.png');">
                     <div class="carousel-text">
-                        <div class="mb-3" style="font-size: 45px">
+                        <div class="carousel-title mb-3" style="font-size: 45px">
                             느린걸음<br>
                             보통날;
                         </div>
-                        <div class="mb-5" style="font-size: 14px; color: #979797;">
+                        <div class="carousel-description mb-5" style="font-size: 14px; color: #979797;">
                             서울 은평 미래유산을 찾아<br>
                             2개월간 촬영하고 멋진 사진을 전시하게 위해<br>
                             1주일 단위로 모여 리뷰를 진행하신<br>
                             50플러스 가까이 사진 동호회팀과 함께 합니다.
                         </div>
-                        <div class="mb-4" style="font-size: 17px">
+                        <div class="carousel-bottom mb-4" style="font-size: 17px">
                             일시 : 2019년 9월 27일(금)~10월 4일(금)<br>
                             장소 : 구산도서관마을<br>
                             참여비 : 없음 - 열린전시
@@ -94,17 +116,17 @@
             <div class="carousel-item">
                 <div class="carousel-bgimg position-relative" style="height: 500px; background: no-repeat center/cover url('images/main-2.png');">
                     <div class="carousel-text">
-                        <div class="mb-3" style="font-size: 45px">
+                        <div class="carousel-title mb-3" style="font-size: 45px">
                             가을하늘에<br>
                             드러나다;
                         </div>
-                        <div class="mb-5" style="font-size: 14px; color: #979797;">
+                        <div class="carousel-description mb-5" style="font-size: 14px; color: #979797;">
                             오래 살아왔기에,<br>
                             많은 시간을 보낸 곳이기에<br>
                             가까운 줄만 알았던 은평의 마을.<br>
                             하지만 몰랐던 마을의 이야기는 늘 많습니다.
                         </div>
-                        <div class="mb-4" style="font-size: 17px">
+                        <div class="carousel-bottom mb-4" style="font-size: 17px">
                             일시 : 2019년 10월 5일(토) 3시~5시<br>
                             장소 : 혁신파크 한평책빵<br>
                             공예체험비 : 3,000원
@@ -118,17 +140,17 @@
             <div class="carousel-item">
                 <div class="carousel-bgimg position-relative" style="height: 500px; background: no-repeat center/cover url('images/main-3.png');">
                     <div class="carousel-text">
-                        <div class="mb-3" style="font-size: 45px">
+                        <div class="carousel-title mb-3" style="font-size: 45px">
                             청년,<br>
                             그 은밀한 대화;
                         </div>
-                        <div class="mb-5" style="font-size: 14px; color: #979797;">
+                        <div class="carousel-description mb-5" style="font-size: 14px; color: #979797;">
                             은평에 살고 있는,<br>
                             또는 활동하고 있는 은밀한 청년들.<br>
                             그들은 어떤 생각을 갖고 살아갈까?<br>
                             또 어떤 생각으로 은평을 바라보고 있을까?
                         </div>
-                        <div class="mb-4" style="font-size: 17px">
+                        <div class="carousel-bottom mb-4" style="font-size: 17px">
                             일시 : 2019년 10월 9일(수) / 저녁7시~9시<br>
                             장소 : 대조동 다용도실 카페<br>
                             참여비 : 없음<br>
@@ -143,16 +165,16 @@
             <div class="carousel-item">
                 <div class="carousel-bgimg position-relative" style="height: 500px; background: no-repeat center/cover url('images/main-4.png');">
                     <div class="carousel-text">
-                        <div class="mb-3" style="font-size: 45px">
+                        <div class="carousel-title mb-3" style="font-size: 45px">
                             은밀하게<br>
                             평범하게<br>
                             통합전시;
                         </div>
-                        <div class="mb-5" style="font-size: 14px; color: #979797;">
+                        <div class="carousel-description mb-5" style="font-size: 14px; color: #979797;">
                             익숙한 일상이 색다름이 되는 처음 시작<br>
                             로컬, 사람들이야기를 만나고 연결되고 다시 확장됩니다
                         </div>
-                        <div class="mb-4" style="font-size: 17px">
+                        <div class="carousel-bottom mb-4" style="font-size: 17px">
                             일시 : 10월 23일 은평마을공동체 행사<br>
                             장소 : 은평구청 1층홀<br>
                             참여비 : 없음 - 열린전시
